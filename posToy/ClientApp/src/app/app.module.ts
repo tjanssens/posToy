2ca43bdf4c4b1288db6bbe72f975e8c46e02ba1e
Tom Janssens <tom@assistonline.eu>
2019-12-22 14:58:11 +0100

    deleted demo directories and upgraded serialize-javascript

--- posToy/ClientApp/src/app/app.module.ts
+++ posToy/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
@@ -29,8 +27,6 @@ import { PaymentDialog } from './payment/payment-dialog.component';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     PaymentDialog
   ],
   imports: [
@@ -39,8 +35,6 @@ import { PaymentDialog } from './payment/payment-dialog.component';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
     ]),
     BrowserAnimationsModule,
     MatSliderModule,
